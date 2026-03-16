5840a6dff7c4398192e4e9b0983200798e7e70f8
Cledson Gean <geandias74@gmail.com>
2026-03-16 00:10:36 -0300

    Add files via upload

--- testes.js
+++ testes.js
@@ -1,117 +1,147 @@
 
-    function calcularTributos(ano, receita, custos, despesas, tipo, cbsAno, ibsAno) {
+let slideAtual = 0;
 
-        let icms = tipo === "vendas" ? 0.19 : 0;
-        let iss = tipo === "servicos" ? 0.05 : 0;
+function mostrarSlide(index) {
 
-        let pis = 0, cofins = 0, ibs = 0, cbs = 0;
+    const slides = document.querySelectorAll(".slide");
 
-        if (ano === 2025) {
-            pis = 0.0165;
-            cofins = 0.076;
-        }
-
-        if (ano === 2026) {
-            pis = 0.0165;
-            cofins = 0.076;
-            ibs = 0.000;
-            cbs = 0.000;
-        }
-
-        if (ano >= 2027 && ano <= 2028) {
-            ibs = 0.001;
-            cbs = cbsAno;
-        }
-
-        if (ano >= 2029 && ano <= 2032) {
-            let fator = (ano - 2028) * 0.10;
-            let baseIBS = (receita - custos - despesas);
-
-            ibs = (ibsAno * fator);
-            icms *= (1 - fator);
-            iss *= (1 - fator);
-            cbs = cbsAno;
-        }
-
-        if (ano === 2033) {
-            ibs = ibsAno;
-            icms = 0;
-            iss = 0;
-            cbs = cbsAno;
-        }
-
-        const debICMS_ISS = receita * (icms + iss);
-        const debPIS = [receita - (receita * 0.19)] * pis;
-        const debCOFINS = [receita - (receita * 0.19)] * cofins;
-        const debIBS = receita * ibs;
-        const debCBS = receita * cbs;
-
-        const baseCred = custos + despesas;
-
-        const credPIS = baseCred * pis;
-        const credCOFINS = baseCred * cofins;
-        const credICMS = baseCred * icms;
-        const credIBS = baseCred * ibs;
-        const credCBS = baseCred * cbs;
-
-        const pisCofinsLiquido = (debPIS + debCOFINS) - (credPIS + credCOFINS);
-        const icmsissLiquido = debICMS_ISS - credICMS;
-
-        let ibsLiquido = debIBS - credIBS;
-
-        if (tipo === "servicos") {
-            if (ano === 2025) {
-                ibsLiquido = 0;
-            }
-
-            if (ano === 2026) {
-                ibsLiquido = 0.001 * (receita - baseCred);
-            }
-
-            if (ano >= 2029) {
-                const redutores = { 2029: 0.90, 2030: 0.80, 2031: 0.70, 2032: 0.60, 2033: 0.00 };
-                ibsLiquido = [(1 - redutores[ano]) * ibsAno] * (receita - baseCred);
-            }
-        }
-
-        const cbsLiquido = debCBS - credCBS;
-        let totalTributos = icmsissLiquido + pisCofinsLiquido + ibsLiquido + cbsLiquido;
-
-        if (ano === 2026) {
-            totalTributos = icmsissLiquido + pisCofinsLiquido;
-        }
-
-        return { icms_iss: icmsissLiquido, pis_cofins: pisCofinsLiquido, ibs: ibsLiquido, cbs: cbsLiquido, total: totalTributos };
+    if (index >= slides.length) {
+        slideAtual = 0;
     }
 
-    function simular() {
+    else if (index < 0) {
+        slideAtual = slides.length - 1;
+    }
 
-        const receitaValor = +document.getElementById("receita").value;
-        const custosValor = +document.getElementById("custos").value;
-        const despesasValor = +document.getElementById("despesas").value;
-        const tipo = document.getElementById("tipo").value;
+    else {
+        slideAtual = index;
+    }
 
-        const tabela = document.getElementById("resultado");
-        tabela.innerHTML = "";
+    slides.forEach(slide => slide.classList.remove("active"));
 
-        for (let ano = 2025; ano <= 2033; ano++) {
+    slides[slideAtual].classList.add("active");
+}
 
-            const cbsAno = document.getElementById(`cbs_${ano}`)?.value || 0;
-            const ibsAno = document.getElementById(`ibs_${ano}`)?.value || 0;
+function mudarSlide(direcao) {
+    mostrarSlide(slideAtual + direcao);
+}
 
-            const t = calcularTributos(
-                ano,
-                receitaValor,
-                custosValor,
-                despesasValor,
-                tipo,
-                +cbsAno,
-                +ibsAno
-            );
 
-            const fco = receitaValor - custosValor - despesasValor - t.total;
 
-            tabela.innerHTML += `
+
+function calcularTributos(ano, receita, custos, despesas, tipo, cbsAno, ibsAno) {
+
+    let icms = tipo === "vendas" ? 0.19 : 0;
+    let iss = tipo === "servicos" ? 0.05 : 0;
+
+    let pis = 0, cofins = 0, ibs = 0, cbs = 0;
+
+    if (ano === 2025) {
+        pis = 0.0165;
+        cofins = 0.076;
+    }
+
+    if (ano === 2026) {
+        pis = 0.0165;
+        cofins = 0.076;
+        ibs = 0.000;
+        cbs = 0.000;
+    }
+
+    if (ano >= 2027 && ano <= 2028) {
+        ibs = 0.001;
+        cbs = cbsAno;
+    }
+
+    if (ano >= 2029 && ano <= 2032) {
+        let fator = (ano - 2028) * 0.10;
+        let baseIBS = (receita - custos - despesas);
+
+        ibs = (ibsAno * fator);
+        icms *= (1 - fator);
+        iss *= (1 - fator);
+        cbs = cbsAno;
+    }
+
+    if (ano === 2033) {
+        ibs = ibsAno;
+        icms = 0;
+        iss = 0;
+        cbs = cbsAno;
+    }
+
+    const debICMS_ISS = receita * (icms + iss);
+    const debPIS = (receita - receita * 0.19) * pis;
+    const debCOFINS = (receita - receita * 0.19) * cofins;
+    const debIBS = receita * ibs;
+    const debCBS = receita * cbs;
+
+    const baseCred = custos + despesas;
+
+    const credPIS = baseCred * pis;
+    const credCOFINS = baseCred * cofins;
+    const credICMS = baseCred * icms;
+    const credIBS = baseCred * ibs;
+    const credCBS = baseCred * cbs;
+
+    const pisCofinsLiquido = (debPIS + debCOFINS) - (credPIS + credCOFINS);
+    const icmsissLiquido = debICMS_ISS - credICMS;
+
+    let ibsLiquido = debIBS - credIBS;
+
+    if (tipo === "servicos") {
+        if (ano === 2025) {
+            ibsLiquido = 0;
+        }
+
+        if (ano === 2026) {
+            ibsLiquido = 0.001 * (receita - baseCred);
+        }
+
+        if (ano >= 2029) {
+            const redutores = { 2029: 0.90, 2030: 0.80, 2031: 0.70, 2032: 0.60, 2033: 0.00 };
+            ibsLiquido = [(1 - redutores[ano]) * ibsAno] * (receita - baseCred);
+        }
+    }
+
+    const cbsLiquido = debCBS - credCBS;
+    let totalTributos = icmsissLiquido + pisCofinsLiquido + ibsLiquido + cbsLiquido;
+
+    if (ano === 2026) {
+        totalTributos = icmsissLiquido + pisCofinsLiquido;
+    }
+
+    return { icms_iss: icmsissLiquido, pis_cofins: pisCofinsLiquido, ibs: ibsLiquido, cbs: cbsLiquido, total: totalTributos };
+}
+
+function simular() {
+
+    const receitaValor = +document.getElementById("receita").value;
+    const custosValor = +document.getElementById("custos").value;
+    const despesasValor = +document.getElementById("despesas").value;
+    const tipo = document.getElementById("tipo").value;
+
+    const tabela = document.getElementById("resultado");
+    tabela.innerHTML = "";
+
+    for (let ano = 2025; ano <= 2033; ano++) {
+
+        const cbsAno = document.getElementById(`cbs_${ano}`)?.value || 0;
+        const ibsAno = document.getElementById(`ibs_${ano}`)?.value || 0;
+
+        const t = calcularTributos(
+            ano,
+            receitaValor,
+            custosValor,
+            despesasValor,
+            tipo,
+            +cbsAno,
+            +ibsAno
+        );
+
+        const fco = receitaValor - custosValor - despesasValor - t.total;
+
+        tabela.innerHTML += `
             <tr>
                 <td>${ano}</td>
                 <td>R$ ${t.icms_iss.toFixed(2)}</td>
@@ -122,8 +152,8 @@
                 <td>R$ ${fco.toFixed(2)}</td>
             </tr>
         `;
-        }
     }
+}
 
 function testeConsistenciaInterna() {
 
@@ -134,7 +164,7 @@ function testeConsistenciaInterna() {
     const despesas = 10000;
     const tipo = "vendas";
 
-    const ano = 2028;
+    const ano = 2033;
 
     const aliquotaBaixa = calcularTributos(ano, receita, custos, despesas, tipo, 0.08, 0.18);
     const aliquotaAlta = calcularTributos(ano, receita, custos, despesas, tipo, 0.12, 0.28);
